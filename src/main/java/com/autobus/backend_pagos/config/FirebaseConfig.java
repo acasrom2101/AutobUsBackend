2e677ea2966a673839a890b7a5f553effc57f44e
iflipper <acasrom2101@iescarrillo.es>
2026-05-02 20:22:05 +0200

    Añadido import

--- src/main/java/com/autobus/backend_pagos/config/FirebaseConfig.java
+++ src/main/java/com/autobus/backend_pagos/config/FirebaseConfig.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
+import java.io.FileInputStream;
 import java.io.InputStream;
 
 @Configuration
